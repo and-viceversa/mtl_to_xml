8bdb362fd2ec08ea7b709bc3b54f94aeb04563d4
and-viceversa <25871157+and-viceversa@users.noreply.github.com>
2019-03-19 09:49:02 -0700

    Version 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mtl_to_xml",
-    version="0.2.1",
+    version="0.2.3",
     packages=find_packages(),
     scripts=['mtl_to_xml.py'],
     author="AdamR",
