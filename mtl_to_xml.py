66df1120e2c14ed7810028039fb75032dd8c8bc2
and-viceversa <25871157+and-viceversa@users.noreply.github.com>
2019-03-27 10:49:34 -0700

    Wrote better comments.

--- mtl_to_xml.py
+++ mtl_to_xml.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Convert Landsat MTL text files to .xml using standard ElementTree
+# Convert Landsat MTL text files to .xml using standard ElementTree.
 # Python 3.7
 
 import os
@@ -9,67 +9,105 @@ import xml.etree.ElementTree as etree
 
 
 def target_mode(file_name):
-    # send a single file to the mtl_to_xml conversion
+    # Send a single file to the mtl_to_xml conversion method.
     mtl_to_xml(file_name)
 
 
 def scan_mode():
-    # walk current directory and sub directories. scan each directory for MTL files and convert them.
+    # Walk current directory and sub directories. Search each directory for MTL files and convert them.
     pwd = os.getcwd()
     for root, subfolders, files in os.walk(pwd):
         scan_dir(root)
 
 
 def scan_dir(pwd):
-    # Builds a dictionary of filepath:filename of all MTL files in a directory.
-    # Sends that dictionary to mtl_to_xml for conversion.
+    # Builds a dictionary filepath:filename of all MTL files in a directory.
+    # Sends that dictionary to the mtl_to_xml conversion method.
 
-    dir_file_list = os.listdir(pwd)  # get list of all files in present working directory
-    mtl_dict = {}                    # dict to hold filepath:filename
+    # Get list of all files in present working directory
+    dir_file_list = os.listdir(pwd)
 
+    # Dictionary to hold filepath:filename
+    mtl_dict = {}
+
+    # Let the user know where the script looked.
     print('Searching in:', pwd)
 
-    for file_name in dir_file_list:  # locate MTL files and place them in our working list
+    # Locate MTL files and place them in a working List
+    for file_name in dir_file_list:
         if '_MTL.txt' in file_name:
+
+            # Let the user know which files the script found.
             print('Found:', file_name)
+
+            # Add filepath:filename to the dictionary
             mtl_dict[pwd+'/'+file_name] = file_name
 
+    # Send the Dictionary of filepath:filename to mtl_to_xml conversion method.
     mtl_to_xml(mtl_dict)
 
 
 def mtl_to_xml(mtl_dict):
-    # converts mtl to xml
+    # Converts Landsat MTL metadata file to XML.
+    # Output the XML in the same directory as the MTL
+
     for key in mtl_dict:
-        new_file = key.replace('.txt', '.xml')   # new .xml file to be created
+        # New .xml file to be created
+        new_file = key.replace('.txt', '.xml')
         try:
-            with fileinput.input(files=key, mode='r') as f:  # fileinput is used instead of open so we can readline()
+            # Fileinput is used here instead of Open so we can readline()
+            with fileinput.input(files=key, mode='r') as f:
+                # Read first line of MTL as root Element.
                 first_line = f.readline().strip()
+                # For it's tree-like structure, MTL files are organized in 'Groups = Group_Name_Here'.
+                # We want to strip out 'Groups = ' and get only the Group name.
                 first_line = first_line[(first_line.index('=')) + 2:]
-                root = etree.Element(first_line)                               # capture first line as root Element
-                current_group = ''                                             # temp var for storing current Element
+                # Create the root of the ElementTree from the first Group in the MTL file.
+                root = etree.Element(first_line)
+
+                # Holding variable for storing current Element
+                current_group = ''
 
                 for line in f:
                     try:
-                        temp = line.strip()                         # remove unneeded whitespace
-                        temp = temp.replace("\"", "")               # remove unneeded double quotes
-                        pre = temp[0:temp.index('=') - 1]           # getting a "key" for xml
-                        post = temp[temp.index('=') + 2:]           # getting a "value" for xml
+                        # Remove unneeded whitespace.
+                        temp = line.strip()
+                        # Remove unneeded double quotes.
+                        temp = temp.replace("\"", "")
+                        # MTL is structured like a series of key:value pairs separated by an '='.
+                        # pre gets the 'key' before the '='
+                        # post get the 'value' after the '='
+                        pre = temp[0:temp.index('=') - 1]
+                        post = temp[temp.index('=') + 2:]
                     except Exception as e:
+                        # A catch-all Exception block isn't a good idea.
+                        # When we hit the end of the file, silently break.
                         print(e)
-                        break                                       # when we hit the end of the file, silently break
+                        break
+                    # MTL files use End_Group to denote the end of an Element.
+                    # When we hit the end of an element, silently continue.
                     if pre == 'END_GROUP':
                         continue
-                    elif pre == 'GROUP' in line:                    # in the mtl file, "groups" are children of root
+                    # MTL files use Group to denote the start of an Element.
+                    elif pre == 'GROUP' in line:
+                        # Add the Group to the ElementTree as a SubElement
+                        etree.SubElement(root, post)
+                        # This temp variable points to the current MTL Group, and is used in the next elif statement.
                         current_group = post
-                        etree.SubElement(root, current_group)
                     elif '=' in line:
-                        temp_element = etree.SubElement(root.find(current_group), pre)  # setting a "key" in xml
-                        temp_element.text = post                                        # setting a "value" in xml
+                        # From the root of the ElementTree, find the current_group and establish a new SubElement.
+                        # Sets the key in key:value.
+                        element = etree.SubElement(root.find(current_group), pre)
+                        # Add text to the SubElement. Sets the value in key:value.
+                        element.text = post
 
-                for element in root:                                # indent for pretty print
+                # Indent the new XML file for pretty print.
+                for element in root:
                     indent(element)
 
+                # Build an ElementTree from the Elements and SubElements created above.
                 xml_tree = etree.ElementTree(root)
+                # Write out the ElementTree to the new .xml file.
                 xml_tree.write(new_file, encoding="us-ascii", xml_declaration=True)
         except FileNotFoundError:
             print('That file does not exist.')
@@ -79,8 +117,8 @@ def mtl_to_xml(mtl_dict):
 
 
 def indent(elem, level=0):
+    # Indent method from Fredrik Lundh
     # https://effbot.org/zone/element-lib.htm#prettyprint
-    # indent method from Fredrik Lundh
 
     i = "\n" + level*"  "
     if len(elem):
@@ -105,7 +143,7 @@ def parse_args():
                         action='store',
                         required=False,
                         help='Target mode converts a single MTL file to XML. \n '
-                             'The script must be run in the same directory as the MTL file. \n '
+                             'The script must be run in the same directory as the MTL file or include a filepath. \n '
                              'Example: mtl_to_xml -t your_landsat_file_MTL.txt \n')
 
     parser.add_argument('-d', '--directory',
@@ -133,10 +171,13 @@ def main():
 
     args = parse_args()
 
+    # Target mode. Converts a single file.
     if args.target:
         target_mode(args.target)
+    # Directory mode. Scans one directory for MTL files and converts them to XML.
     elif args.directory:
         scan_dir(os.getcwd())
+    # Scans top directory and all subdirectories. Converts the MTL files it finds to XML.
     elif args.scan:
         scan_mode()
 
